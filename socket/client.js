c17ca96f2eb56592dc358265c27a8fe58ce370f0
Nikhilmalani7777777 <63990502+Nikhilmalani7777777@users.noreply.github.com>
2022-05-29 12:34:37 +0000

    Update client.js

--- socket/client.js
+++ socket/client.js
@@ -1,6 +1,6 @@
 const net = require('net');
 
-const client = net.connect({port:7777}, function(){
+const client = net.connect({port:7777, host:'127.0.0.1'}, function(){//default host will be localhost(if we not define)
 
 	client.on('data',function(data){
 
